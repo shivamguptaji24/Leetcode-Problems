aa4c6ae06c23ccd6f1ef0c950c4e37b2240c1548
Shivam Gupta <shivamguptaji24@gmail.com>
2025-03-24 23:15:28 +0530

    Leetcode 3169 - 24 Mar 2025

--- CountDaysWithoutMeetings.java
+++ CountDaysWithoutMeetings.java
@@ -137,3 +137,54 @@ Summary
 ✔ Tracked occupied days dynamically to avoid iterating over large `days` values  
 ✔ Final count of available days is computed in `O(N log N)`  
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 15ms runtime which is the lowest time in this problem
+*/
+
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        int freeDays = days;
+        TreeMap<Integer, Integer> meetingDays = new TreeMap<>();
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            // System.out.printf("Meeting: (%d, %d)\n", start, end);
+            int overlapDays = 0;
+
+            Map.Entry<Integer, Integer> previousMeetingDays = meetingDays.floorEntry(start);
+            if (previousMeetingDays != null && previousMeetingDays.getValue() >= start - 1) {
+                if (previousMeetingDays.getValue() >= end) continue; // Complete overlap
+                overlapDays = previousMeetingDays.getValue() - previousMeetingDays.getKey() + 1;
+                // System.out.printf("Overlapping previous: (%d, %d)\n", previousMeetingDays.getKey(), previousMeetingDays.getValue());
+                start = previousMeetingDays.getKey();
+            }
+
+            Map.Entry<Integer, Integer> nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            while (nextMeetingDays != null && nextMeetingDays.getKey() <= end + 1) {
+                meetingDays.remove(nextMeetingDays.getKey());
+                overlapDays += nextMeetingDays.getValue() - nextMeetingDays.getKey() + 1;
+                // System.out.printf("Overlapping next: (%d, %d)\n", nextMeetingDays.getKey(), nextMeetingDays.getValue());
+                if (nextMeetingDays.getValue() >= end) {
+                    end = nextMeetingDays.getValue();
+                    break;
+                }
+                nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            }
+
+            meetingDays.put(start, end);
+            freeDays -= (end - start + 1) - overlapDays;
+            // System.out.printf("Result: (%d, %d)\n", start, end);
+            // System.out.printf("FreeDays - ((%d - %d + 1) - %d) = %d\n\n", end, start, overlapDays, freeDays);
+            if (freeDays == 0) break;
+        }
+        return freeDays;
+
+    }
+}
+
+/*
+Visualization of the above code
+ 
