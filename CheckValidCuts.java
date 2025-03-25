088f617a1fec3edc5138d1d5bca666434f49405e
Shivam Gupta <shivamguptaji24@gmail.com>
2025-03-25 23:54:13 +0530

    Leetcode 3394 - 25 Mar 2025

--- CheckValidCuts.java
+++ CheckValidCuts.java
@@ -186,3 +186,48 @@ Overall Complexity: `O(K log K)`, which is optimal.
 ✅ Efficient O(K log K) solution  
 ✅ Fails test case where no valid cut exists (Expected Output: `false`)
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 35ms runtime which is the lowest time in this problem
+*/
+
+public class Solution {
+    private static final int MASK = (1 << 30) - 1;
+
+    public boolean checkValidCuts(int m, int[][] rectangles) {
+        int n = rectangles.length;
+        long[] start = new long[n];
+        for (int i = 0; i < n; i++) {
+            start[i] = ((long) rectangles[i][1] << 32) + rectangles[i][3];
+        }
+        Arrays.sort(start);
+        if (validate(start)) {
+            return true;
+        }
+        for (int i = 0; i < n; i++) {
+            start[i] = ((long) rectangles[i][0] << 32) + rectangles[i][2];
+        }
+        Arrays.sort(start);
+        return validate(start);
+    }
+
+    private boolean validate(long[] arr) {
+        int cut = 0;
+        int n = arr.length;
+        int max = (int) arr[0] & MASK;
+        for (int i = 0; i < n; i++) {
+            int start = (int) (arr[i] >> 32);
+            if (start >= max && ++cut == 2) {
+                return true;
+            }
+            max = Math.max(max, (int) (arr[i] & MASK));
+        }
+        return false;
+    }
+}
+
+/*
+Visualization of the above code
+ 
