e45ba18420a30fd7ecc900faa4e7a7336f7e5017
Shivam Gupta <shivamguptaji24@gmail.com>
2025-03-27 22:45:54 +0530

    Leetcode 2780 - 27 Mar 2025

--- MinimumIndex.java
+++ MinimumIndex.java
@@ -167,3 +167,50 @@ Output: 4
 ---
 ✅ Efficiently finds the minimum index for a valid split in `O(n)`. 🚀
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 8ms runtime which is the lowest time in this problem
+*/
+
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int n=nums.size();
+        int dominant=-1;
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                dominant=nums.get(i);
+                cnt++;
+            }
+            else if(nums.get(i)==dominant){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int maxC=0;
+        for(int i=0;i<n;i++){
+            if(nums.get(i)==dominant){
+                maxC++;
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(nums.get(i)==dominant){
+                c++;
+            }
+            if(c*2>(i+1) && (maxC-c)*2>(n-i-1)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+/*
+Visualization of the above code
+ 
