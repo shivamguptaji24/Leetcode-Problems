c51c85a65b40990d3ba50ee2ceb6ec5a12f3bd11
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-01 20:50:09 +0530

    Leetcode 2140 - 01 Apr 2025

--- QuestionsWithBrainPower.java
+++ QuestionsWithBrainPower.java
@@ -175,3 +175,42 @@ Key Observations
 2. Two choices: Either skip the question or solve it and jump.
 3. Efficient: O(n) time, O(n) space (or O(1) space if optimized).
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 1ms runtime which is the lowest time in this problem
+*/
+
+class Solution {
+    private long calcMaxPoints(int[][] questions, int index, int n, long[] dp) {
+        if (index == n) return 0;
+        if (dp[index] != -1) return dp[index];
+        long take = questions[index][0];
+        int newIndex = index + questions[index][1] + 1;
+        if (newIndex < n) take += calcMaxPoints(questions, index + questions[index][1] + 1, n, dp);
+        long notTake = calcMaxPoints(questions, index + 1, n, dp);
+
+        return dp[index] = Math.max(take, notTake);
+    }
+    static {
+        int[][] arr = {{3,2},{4,3},{4,4},{2,5}};
+        for (int i = 0 ; i < 100 ; i++) mostPoints(arr);
+    }
+    public static long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];
+        // Arrays.fill(dp, -1);
+        // return calcMaxPoints(questions, 0, n, dp);
+        dp[n - 1] = questions[n - 1][0];
+        for (int i = n - 2 ; i >= 0 ; i--) {
+            long take = questions[i][0];
+            int newIndex = i + questions[i][1] + 1;
+            take += (newIndex < n) ? dp[newIndex] : 0;
+            long notTake = dp[i + 1];
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[0];
+    }
+}
