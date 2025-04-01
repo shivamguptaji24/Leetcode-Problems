02697fc9d8c225b40a73acd14125aad4de8d849a
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-01 20:43:27 +0530

    Leetcode 2140 - 01 Apr 2025

--- QuestionsWithBrainPower.java
+++ QuestionsWithBrainPower.java
@@ -37,3 +37,23 @@ Constraints:
 questions[i].length == 2
 1 <= pointsi, brainpoweri <= 105
 */
+
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1]; // Extra space for handling out-of-bounds cases
+
+        for (int i = n - 1; i >= 0; i--) {
+            int points = questions[i][0];
+            int brainpower = questions[i][1];
+            
+            // If solving, move to the next available question (or end if out of bounds)
+            long nextIndexPoints = (i + brainpower + 1 < n) ? dp[i + brainpower + 1] : 0;
+
+            // Choose the best option: Skip or Solve
+            dp[i] = Math.max(dp[i + 1], points + nextIndexPoints);
+        }
+        
+        return dp[0]; // Maximum points from question 0
+    }
+}
