e8cde64973e856c2f06c7de6c86eed855cc8946a
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-03 18:41:05 +0530

    Leetcode 2874 - 03 Apr 2025

--- MaximumTripletValueII.java
+++ MaximumTripletValueII.java
@@ -126,3 +126,17 @@ Space Complexity: `O(1)`
 /*
 This is the solution that takes only 1ms runtime which is the lowest time in this problem
 */
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long maxProduct=0;
+        int maxDiff=0;
+        int maxNum=0;
+        for(int num: nums){
+            maxProduct = Math.max(maxProduct, (long)maxDiff*num);
+            maxNum = Math.max(maxNum, num);
+            maxDiff = Math.max(maxDiff, maxNum-num);
+        }
+        return maxProduct>0?maxProduct:0;
+    }
+}
