83eccb4f88e2dcd5dd25ce27386fe14ee750ecd1
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-05 22:51:18 +0530

    Leetcode 1863 - 05 Apr 2025

--- SubsetXORSum.java
+++ SubsetXORSum.java
@@ -142,3 +142,25 @@ dfs(0, 0) = dfs(1, 1) + dfs(1, 0)
 This is the Brute Force Approach of this problem (BitMasking).
 */
 
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+
+        // iterate over all possible subsets
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int xor = 0;
+
+            for (int i = 0; i < n; i++) {
+                // if the i-th bit is set, include nums[i]
+                if ((mask & (1 << i)) != 0) {
+                    xor ^= nums[i];
+                }
+            }
+
+            total += xor;
+        }
+
+        return total;
+    }
+}
