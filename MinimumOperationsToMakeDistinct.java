edc0233d365d50dbb00f4f3b8d38fff98ea373e3
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-08 22:38:38 +0530

    Leetcode 3396 - 08 Apr 2025

--- MinimumOperationsToMakeDistinct.java
+++ MinimumOperationsToMakeDistinct.java
@@ -174,6 +174,18 @@ return 2;
 /*-------------------------------------------------------------------------------------------------------------------------*/
 
 /*
-This is the solution that takes only 4ms runtime which is the lowest time in this problem
+This is the solution that takes only 0ms runtime which is the lowest time in this problem
 */
 
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[101];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(seen[nums[i]])
+                return i / 3 + 1;
+            else seen[nums[i]] = true;
+        }
+        return 0;
+    }
+}
