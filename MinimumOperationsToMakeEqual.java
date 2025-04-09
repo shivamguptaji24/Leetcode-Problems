43a1d19a625ddbb27a398a7cb6b58026e24ede40
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-09 23:30:37 +0530

    Leetcode 3375 - 09 Apr 2025

--- MinimumOperationsToMakeEqual.java
+++ MinimumOperationsToMakeEqual.java
@@ -301,3 +301,23 @@ This code:
 /*
 This is the solution that takes only 1ms runtime which is the lowest time in this problem
 */
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        boolean[] has = new boolean[101];
+
+        for (int num : nums) {
+            has[num] = true;
+        }
+        int cnt = 0;
+        for (int i = 0; i < has.length; i++) {
+            if (!has[i]) continue;
+
+            if (has[i] && i < k)
+                return -1;
+            else if (has[i] && i > k)
+                cnt++;
+        }
+        return cnt;
+    }
+}
