4e7925b471396ec8cf95f4dba1ebfd4a2221e563
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-11 23:39:14 +0530

    Leetcode 2843 - 11 Apr 2025

--- CountSymmetricIntegers.java
+++ CountSymmetricIntegers.java
@@ -22,3 +22,34 @@ Constraints:
 
 1 <= low <= high <= 10^4
 */
+
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+
+        for (int num = low; num <= high; num++) {
+            String s = Integer.toString(num);
+            int len = s.length();
+
+            if (len % 2 == 0) {
+                int half = len / 2;
+                int sum1 = 0, sum2 = 0;
+
+                for (int i = 0; i < half; i++) {
+                    sum1 += s.charAt(i) - '0';
+                    sum2 += s.charAt(i + half) - '0';
+                }
+
+                if (sum1 == sum2) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+/*
+Visualization of the above code
+ 
