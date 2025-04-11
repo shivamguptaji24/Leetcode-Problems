7dec9d7f2fa9b4c3c0cf75fbc006c51cbf57db32
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-11 23:46:12 +0530

    Leetcode 2843 - 11 Apr 2025

--- CountSymmetricIntegers.java
+++ CountSymmetricIntegers.java
@@ -151,3 +151,53 @@ The symmetric integers between 1200 and 1230 are:
 
 So, the result is: 4
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 2ms runtime which is the time in this problem
+*/
+
+class Solution {
+    private static final short[] symCount = new short[10_001];
+    
+    
+    public int countSymmetricIntegers(int low, int high) {
+        if (symCount[11] == 0)  buildCounts();
+        return symCount[high] - symCount[low - 1];
+    }
+    
+    
+    // One time build of the array of how many symmetric numbers 
+    // exist at or below any index value.  After the first 
+    // leetcode test case, this will not be called for the 
+    // remainder of the submit test cases.  The values in 
+    // symCount[] will be preserved between a submit's test cases.
+    private void buildCounts() {
+        // Fill in 0 to 99.
+        for (int num = 11; num <= 99; num++)
+            symCount[num] = (short)(num / 11);
+        
+        // Fill in 100 to 999.  No symmetric numbers in this range 
+        // since odd number of digits, to just copy the count for 
+        // symCount[99].
+        short prev = symCount[99];
+        for (int num = 100; num <= 999; num++)
+            symCount[num] = prev;
+        
+        // Fill in 1000 to 9999
+        prev = symCount[999];
+        int idx = 1000;
+        for (int high10 = 1; high10 <= 9; high10++) {
+            for (int high1 = 0; high1 <= 9; high1++) {
+                final int highSum = high10 + high1;
+                for (int low10 = 0; low10 <= 9; low10++) 
+                    for (int low1 = 0; low1 <= 9; low1++) 
+                        symCount[idx++] = (short)((highSum == low10 + low1) ? ++prev : prev);
+            }
+        }
+        
+        // Fill in 10_000.
+        symCount[10_000] = symCount[9999];
+    }
+}
