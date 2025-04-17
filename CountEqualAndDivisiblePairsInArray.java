1dcefd5015a39d0c746b5f29ae4d0eacf24fbb73
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-17 23:30:40 +0530

    Leetcode 2176 - 17 Apr 2025

--- CountEqualAndDivisiblePairsInArray.java
+++ CountEqualAndDivisiblePairsInArray.java
@@ -109,3 +109,28 @@ for (int i = 0; i < n; i++) {
 
 ---
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 1ms runtime which is the lowest time in this problem.
+*/
+
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        return helper(nums,k,0);
+    }
+
+private static int helper(int[] nums, int k, int i){
+    if(i>=nums.length){
+        return 0;
+    }
+    int count = 0;
+    for(int j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j] && (i*j)%k==0){
+            count++;
+        }
+    }
+    return count+helper(nums,k,i+1);
+  }
+}
