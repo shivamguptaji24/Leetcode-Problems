acecb41edf7b90ba631396add0a26d966bdb26e1
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-20 21:04:39 +0530

    Leetcode 781 - 20 Apr 2025

--- RabbitsInForest.java
+++ RabbitsInForest.java
@@ -121,3 +121,27 @@ return 5;
 
 ---
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 0ms runtime which is the lowest time in this problem.
+*/
+
+class Solution {
+    public int numRabbits(int[] answers) {
+
+        int n = answers.length;
+        int[] count = new int[1000];
+        
+        int ans = 0;
+        for(int x:answers){
+            if(++count[x] == 1)
+                ans += x + 1;
+            if(count[x] == x + 1)
+                count[x] = 0;
+        }
+        
+        return ans;
+    }
+}
