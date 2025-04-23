2500c11743e5c8ef7ccd5f3fa6bb44484730502d
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-23 22:49:57 +0530

    Leetcode 1399 - 23 Apr 2025

--- CountLargestGroup.java
+++ CountLargestGroup.java
@@ -25,3 +25,37 @@ Constraints:
 
 1 <= n <= 104
 */
+
+class Solution {
+    public int countLargestGroup(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = digitSum(i);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            max = Math.max(max, map.get(sum)); // keep track of largest group size
+        }
+
+        int count = 0;
+        for (int size : map.values()) {
+            if (size == max) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+        private int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
+
+/*
+Visualization of the above code
