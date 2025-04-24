b7aebee3cf2067ce12339d4fdaafa7206e5adcc8
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-24 23:27:19 +0530

    Leetcode 2799 - 24 Apr 2025

--- CountCompleteSubarrays.java
+++ CountCompleteSubarrays.java
@@ -273,3 +273,35 @@ So, total time is approximately O(n).
 
 ---
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 2ms runtime which is the lowest time in this problem.
+*/
+
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        boolean[] exists = new boolean[2001];
+        int distinct = 0;
+        for( int n : nums ){
+            if( !exists[n] ){
+                exists[n] = true;
+                distinct++;
+            }
+        }
+        int[] freq = new int[2001];
+        int count = 0, n = nums.length;
+        int sub = 0;
+        for( int start = 0, end = 0; end < n; end++ ){
+            if( freq[ nums[ end ] ]++ == 0 )
+                count++;
+            while( count == distinct ){
+                sub += n - end;
+                if( freq[ nums[ start ++ ] ]-- == 1 )
+                    count--;
+            }
+        }
+        return sub;
+    }
+}
