88cd9654ed913d5d7eb6f9f2c117e5e23df5d14b
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-25 22:28:38 +0530

    Leetcode 2845 - 25 Apr 2025

--- CountInterestingSubarrays.java
+++ CountInterestingSubarrays.java
@@ -185,3 +185,35 @@ Answer matches the example ✅
 This is the solution that takes only 7ms runtime which is the lowest time in this problem.
 */
 
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+
+        int n = nums.size();
+        if(k > n) return 0;
+         
+        int[] count = new int[n + 1];
+        count[0] = 1;
+
+        long ans = 0;
+        int sum = 0;
+        for(int x:nums){
+            x %= modulo;
+            if(x == k)
+                 ++sum;
+
+            sum %= modulo;
+            int r = sum - k;
+            if(r < 0) r += modulo;
+            if(r < n)
+               ans += count[r];
+
+            count[sum]++;
+        }
+        
+        return ans;
+    }
+}
+
+/*
+Visualization of the above code
+ 
