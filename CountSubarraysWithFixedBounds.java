9a70f62c8dc98de302d5633031546972e64e9240
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-26 22:25:03 +0530

    Leetcode 2444 - 26 Apr 2025

--- CountSubarraysWithFixedBounds.java
+++ CountSubarraysWithFixedBounds.java
@@ -157,3 +157,21 @@ That's why:
 This is the solution that takes only 4ms runtime which is the lowest time in this problem.
 */
 
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int left = -1, min = -1, max = -1, n = nums.length;
+        long count = 0l;
+        for (int i = 0; i < n; i++) 
+            if (nums[i] < minK || nums[i] > maxK) 
+                left = min = max = i;
+            else {
+                min = nums[i] == minK ? i : min;
+                max = nums[i] == maxK ? i : max;
+                count += (min < max ? min : max) - left;
+            } 
+        return count;
+    }
+}
+
+/*
+Visualization of the above code
