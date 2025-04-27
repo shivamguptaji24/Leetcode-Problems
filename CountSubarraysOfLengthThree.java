719450d56f46645c2e6e66b0808fde0514277e49
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-27 23:33:15 +0530

    Leetcode 3392 - 27 Apr 2025

--- CountSubarraysOfLengthThree.java
+++ CountSubarraysOfLengthThree.java
@@ -31,3 +31,15 @@ Constraints:
 3 <= nums.length <= 100
 -100 <= nums[i] <= 100
 */
+
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int ans = 0;
+        for (int i = 1; i + 1 < nums.length; ++i) {
+            if ((nums[i - 1] + nums[i + 1]) * 2 == nums[i]) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
