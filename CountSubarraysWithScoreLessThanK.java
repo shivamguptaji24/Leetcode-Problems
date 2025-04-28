9d9d3e32dce4a244d623dc23a6e0ae3560fe6c21
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-28 22:57:56 +0530

    Leetcode 2302 - 28 Apr 2025

--- CountSubarraysWithScoreLessThanK.java
+++ CountSubarraysWithScoreLessThanK.java
@@ -122,3 +122,21 @@ Initial: []
 This is the solution that takes only 3ms runtime which is the second lowest time in this problem.
 */
 
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int n = nums.length ; 
+        long sum = 0 ;
+        long count = 0 ; 
+        int right = 0 , left = 0 ; 
+        while(right < n){
+            sum += nums[right]; 
+            while(left <= right && sum * (right - left + 1) >= k){
+                sum -= nums[left]; 
+                left ++; 
+            }
+            count += (right - left + 1); 
+            right ++ ; 
+        }
+        return count; 
+    }
+}
