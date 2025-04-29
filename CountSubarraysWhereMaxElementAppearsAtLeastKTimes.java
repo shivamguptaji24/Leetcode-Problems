5013e3d1e090b6fd736422a4a8faff115ba580c6
Shivam Gupta <shivamguptaji24@gmail.com>
2025-04-29 23:41:56 +0530

    Leetcode 2962 - 29 Apr 2025

--- CountSubarraysWhereMaxElementAppearsAtLeastKTimes.java
+++ CountSubarraysWhereMaxElementAppearsAtLeastKTimes.java
@@ -181,3 +181,36 @@ count = 6
 This is the solution that takes only 4ms runtime which is the lowest time in this problem.
 */
 
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        
+        // Step 1: Find the maximum element
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        
+        int i = 0;
+        int countmax = 0;
+        long result = 0;
+        
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == max) {
+                countmax++;
+            }
+            
+            while (countmax >= k) {
+                result += (n - j);
+                if (nums[i] == max) {
+                    countmax--;
+                }
+                i++;
+            }
+        }
+        
+        return result;
+    }
+}
