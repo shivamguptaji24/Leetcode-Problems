429428d8ccdf75397d01379931d521b536274fde
Shivam Gupta <shivamguptaji24@gmail.com>
2025-05-03 23:20:04 +0530

    Leetcode 1007 - 03 May 2025

--- MinimumDominoRotationsForEqualRow.java
+++ MinimumDominoRotationsForEqualRow.java
@@ -147,3 +147,36 @@ Track how many rotations needed
 Return minimum number of rotations
 ```
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 4ms runtime which is the most used time in this problem.
+*/
+
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int count1 = solve(tops, bottoms, tops[0]);
+        if(count1 != -1)
+            return count1;
+        return solve(tops, bottoms, bottoms[0]);
+        
+    }
+    int solve(int[] tops, int[] bottoms, int target){
+        int flipTop = 0;
+        int flipBottom = 0;
+        int n = tops.length;
+
+        for(int i=0;i<n;i++){
+            if(tops[i] != target && bottoms[i] != target)
+                return -1;
+            else if(tops[i] != target){
+                flipTop++;
+            }else if(bottoms[i] != target){
+                flipBottom++;
+            }
+        }
+        int res = Math.min(flipTop, flipBottom);
+        return res;
+    }
+}
