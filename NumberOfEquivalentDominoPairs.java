6e8799092e45835a9e19fb2074eb26c5d8c26b11
Shivam Gupta <shivamguptaji24@gmail.com>
2025-05-04 14:40:21 +0530

    Leetcode 1128 - 04 May 2025

--- NumberOfEquivalentDominoPairs.java
+++ NumberOfEquivalentDominoPairs.java
@@ -88,3 +88,24 @@ We found 1 pair of equivalent dominoes: `[1,2]` and `[2,1]`.
 
 ---
 */
+
+/*-------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+This is the solution that takes only 3ms runtime which is the most used time in this problem.
+*/
+
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+         int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
+            int val = domino[0] < domino[1]
+                ? domino[0] * 10 + domino[1]
+                : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+}
