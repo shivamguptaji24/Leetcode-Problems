e5e06a46f008aa065c955de639a6d0c2e7b055c0
Shivam Gupta <shivamguptaji24@gmail.com>
2025-05-04 14:38:30 +0530

    Leetcode 1128 - 04 May 2025

--- NumberOfEquivalentDominoPairs.java
+++ NumberOfEquivalentDominoPairs.java
@@ -22,3 +22,19 @@ Constraints:
 dominoes[i].length == 2
 1 <= dominoes[i][j] <= 9
 */
+
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] count = new int[100];  // Since max key = 9*10 + 9 = 99
+        int result = 0;
+
+        for (int[] domino : dominoes) {
+            int a = domino[0], b = domino[1];
+            int key = a < b ? a * 10 + b : b * 10 + a;
+            result += count[key];
+            count[key]++;
+        }
+
+        return result;
+    }
+}
