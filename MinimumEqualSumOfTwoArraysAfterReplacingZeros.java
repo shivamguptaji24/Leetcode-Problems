1106b3ff45069bddaa60b21276187b6b8eae38cb
Shivam Gupta <shivamguptaji24@gmail.com>
2025-05-10 17:26:13 +0530

    Leetcode 2918 - 10 May 2025

--- MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
+++ MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -27,3 +27,21 @@ Constraints:
 1 <= nums1.length, nums2.length <= 105
 0 <= nums1[i], nums2[i] <= 106
 */
+
+class Solution {
+  public long minSum(int[] nums1, int[] nums2) {
+    final long sum1 = Arrays.stream(nums1).asLongStream().sum();
+    final long sum2 = Arrays.stream(nums2).asLongStream().sum();
+    final long zero1 = Arrays.stream(nums1).filter(num -> num == 0).count();
+    final long zero2 = Arrays.stream(nums2).filter(num -> num == 0).count();
+    if (zero1 == 0 && sum1 < sum2 + zero2)
+      return -1;
+    if (zero2 == 0 && sum2 < sum1 + zero1)
+      return -1;
+    return Math.max(sum1 + zero1, sum2 + zero2);
+  }
+}
+
+/*
+Visualization of the above code
+ 
