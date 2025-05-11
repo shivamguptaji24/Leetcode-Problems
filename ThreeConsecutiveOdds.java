03e1955948a85cfd82a34a2af79b542757ce63ce
Shivam Gupta <shivamguptaji24@gmail.com>
2025-05-11 23:40:52 +0530

    Leetcode 1550 - 11 May 2025

--- ThreeConsecutiveOdds.java
+++ ThreeConsecutiveOdds.java
@@ -22,3 +22,14 @@ Constraints:
 1 <= arr.length <= 1000
 1 <= arr[i] <= 1000
 */
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
